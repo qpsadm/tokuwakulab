ebc3e46ad8652211ded84181b5f1f31ca6f5519e
Nakagawa Mana <mn.nkgw.246824@gmail.com>
2025-02-28 19:04:47 +0900

    3ファイル編集
    3ファイル編集しました
    
    中川真奈

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -3,15 +3,15 @@
 // ①シンプルな実装（toggleClassで実装）
 $(function () {
     // ボタンがクリックされたら発動
-    $(".hamburger_btn").on("click", function () {
+    $(".hamburger").on("click", function () {
         //     // ドロワーメニュー部分のアニメーション
         $(".menu").toggleClass("is-active");
         //     // ボタン部分のアニメーション
-        $(".hamberger_btn").toggleClass("is-active");
+        $(".hamberger").toggleClass("is-active");
     });
     $(".menu li").click(function () {
         $(".menu").removeClass("is-active");
-        $(".hamburger_btn").removeClass("is-active");
+        $(".hamburger").removeClass("is-active");
     });
 });
 
