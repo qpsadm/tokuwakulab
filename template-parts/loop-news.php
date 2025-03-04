e23d4ec479eef3510628aa3962c7e7d06ac3b66b
ogasahara-h <ogasaharahitomi@gmail.com>
2025-03-04 15:22:21 +0900

    Update loop-news.php
    お知らせ一覧のクラス名を変更しました。

--- template-parts/loop-news.php
+++ template-parts/loop-news.php
@@ -1,39 +1,39 @@
-<section id="post-<?php the_ID(); ?>" <?php post_class('cardList_item'); ?>>
-    <a href="<?php the_permalink(); ?>" class="card">
-        <!-- カテゴリー設定 -->
-        <?php $categories = get_the_category(); ?>
-        <?php if ($categories) : ?>
-        <div class="card_label">
-            <?php foreach ($categories as $key => $category) : ?>
-            <span class="">
-                <!-- カテゴリー出力 -->
-                <?php //echo $category->name;
-                        ?>
-            </span>
-            <?php endforeach; ?>
-        </div>
-        <?php endif; ?>
+<!-- カテゴリー設定 -->
+<!-- <?php $categories = get_the_category(); ?>
+<?php if ($categories) : ?>
+<div class="">
+    <?php foreach ($categories as $key => $category) : ?>
+    <span class="">
+         カテゴリー出力
+        <?php //echo $category->name;
+        ?>
+    </span>
+    <?php endforeach; ?>
+</div>
+<?php endif; ?> -->
 
-        <!-- <div class="card_pic"> -->
-        <?php //if (has_post_thumbnail()) :
-        ?>
-        <!-- アイキャッチ画像があった場合は、表示 -->
-        <?php //the_post_thumbnail('medium');
-        ?>
-        <?php //else :
-        ?>
-        <!-- アイキャッチ画像が設定していない場合は、noimage.pngを表示 -->
-        <!-- <img src="<?php //echo get_template_directory_uri();
-                        ?>/assets/img/common/noimage.png" alt=""> -->
-        <?php //endif;
-        ?>
+<!-- <div class="card_pic"> -->
+<?php //if (has_post_thumbnail()) :
+?>
+<!-- アイキャッチ画像があった場合は、表示 -->
+<?php //the_post_thumbnail('medium');
+?>
+<?php //else :
+?>
+<!-- アイキャッチ画像が設定していない場合は、noimage.pngを表示 -->
+<!-- <img src="<?php //echo get_template_directory_uri();
+                ?>/assets/img/common/noimage.png" alt=""> -->
+<?php //endif;
+?>
 
-        <!-- </div> -->
-        <div class="card_body">
+<!-- </div> -->
+<!-- <div class="post_list_item_wrap">
             <h2 class="card_title"><?php the_title(); ?></h2>
             <time datetime="<?php the_time('Y-m-d') ?>">更新日：
                 <?php the_time('Y年m月d日(l)') ?>
             </time>
-        </div>
-    </a>
-</section>
+        </div> -->
+<div class="post_list_item_wrap">
+    <span><?php the_time('Y年m月d日(l)') ?></span>
+    <a href="<?php the_permalink(); ?>"><span class="post_list_item"><?php the_title(); ?></span></a>
+</div>
