23075d2eaf5e7ddad4867332fb607e3245627a34
fukuchen62 <fukuchen62@gmail.com>
2025-03-06 14:31:19 +0900

    Update page-mypage.php
    お気に入りを修正した
    
    小笠原

--- page-mypage.php
+++ page-mypage.php
@@ -6,11 +6,8 @@ if (function_exists('get_user_favorites')) {
     $favorites = get_user_favorites();
     krsort($favorites); //連想配列をキーで昇順ソート
 
-    //print_r($favorites);
-
     // お気に入りデータを取得
     if ($favorites) {
-
         $args = [
             'post_type' => 'event', //イベント投稿
             'posts_per_page' => -1, //全部表示
@@ -20,8 +17,11 @@ if (function_exists('get_user_favorites')) {
             'orderby'        => 'meta_value', // カスタムフィールドの値で並び替え
             'order'          => 'ASC',    // 昇順（古い順）
         ];
-
+        // サブクエリ生成
         $the_query = new WP_Query($args);
+
+        // サブクエリの件数
+        $count = $the_query->found_posts;
     }
 }
 ?>
@@ -50,50 +50,57 @@ if (function_exists('get_user_favorites')) {
                 Cookieの削除に伴いブックマークした情報も削除されます。</p>
         </div>
 
+        <?php if ($favorites): ?>
+            <section>
+                <div class="favorite_result">
+                    <!-- 件数表示 -->
+                    <div>
+                        <span><?php echo $count; ?></span>&nbsp;<span>件</span>
+                    </div>
 
-        <section>
-            <div class="favorite_result">
-                <!-- 件数表示 -->
-                <div>
-                    <span><?php echo $the_query->found_posts; ?></span>&nbsp;<span>件</span>
+                    <!-- 表示順切り替え -->
+                    <div>
+                        <span>締め切り順</span>
+                    </div>
                 </div>
 
-                <!-- 表示順切り替え -->
-                <div>
-                    <span>締め切り順</span>
-                </div>
-            </div>
+                <!-- イベントカード型 -->
+                <ul class="top_event_list">
 
-            <!-- イベントカード型 -->
-            <ul class="top_event_list">
+                    <?php if ($the_query->have_posts()) : ?>
+                        <?php while ($the_query->have_posts()) :  $the_query->the_post(); ?>
 
-                <?php if ($the_query->have_posts()) : ?>
-                    <?php while ($the_query->have_posts()) :  $the_query->the_post(); ?>
+                            <li>
 
-                        <li>
+                                <!-- テンプレートパーツloop-event.phpを読み込む -->
+                                <?php get_template_part('template-parts/loop', 'event') ?>
 
-                            <!-- テンプレートパーツloop-event.phpを読み込む -->
-                            <?php get_template_part('template-parts/loop', 'event') ?>
-
-                        </li>
-                    <?php endwhile;
-                    wp_reset_postdata(); ?>
-                <?php endif; ?>
-
-            </ul>
-            <!-- 改訂ページネーション -->
-            <div class="pagenation">
-
-                <div class="wp-pagenavi">
-                    <?php if (function_exists('wp_pagenavi')): ?>
-
-                        <?php wp_pagenavi(); ?>
-
+                            </li>
+                        <?php endwhile;
+                        wp_reset_postdata(); ?>
                     <?php endif; ?>
-                </div>
-            </div>
 
-        </section>
+                </ul>
+                <!-- 改訂ページネーション -->
+                <div class="pagenation">
+
+                    <div class="wp-pagenavi">
+                        <?php if (function_exists('wp_pagenavi')): ?>
+
+                            <?php wp_pagenavi(); ?>
+
+                        <?php endif; ?>
+                    </div>
+                </div>
+
+            </section>
+        <?php else: ?>
+            <section>
+                <div class="favorite_result">
+                    <p>ブックマークはありません。</p>
+                </div>
+            </section>
+        <?php endif; ?>
     </div>
 
 </main>
