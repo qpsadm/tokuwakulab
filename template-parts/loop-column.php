b9efaa3b67e9ebdef6d004e80ca3103ebfb9e3a4
mimemura <fengcunweiyang@gmail.com>
2025-03-07 17:40:17 +0900

    編集しました。
    編集しました。
    
    峯村

--- template-parts/loop-column.php
+++ template-parts/loop-column.php
@@ -3,18 +3,18 @@
         <div class="cardlist_inner">
             <div class="cardlist_flex">
                 <?php if (has_post_thumbnail()) : ?>
-                <!-- アイキャッチ画像があった場合は、表示 -->
-                <?php the_post_thumbnail('medium'); ?>
+                    <!-- アイキャッチ画像があった場合は、表示 -->
+                    <?php the_post_thumbnail('medium'); ?>
                 <?php else : ?>
-                <!-- アイキャッチ画像が設定していない場合は、noimage.pngを表示 -->
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/noimage.png" alt="">
+                    <!-- アイキャッチ画像が設定していない場合は、noimage.pngを表示 -->
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/noimage.png" alt="">
                 <?php endif; ?>
                 <div class="cardlist_toptext">
                     <span><time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y年m月d日'); ?></time></span>
                     <h3>
                         <!-- タイトルの表示を10文字に制限 -->
-                        <?php if (mb_strlen($post->post_title) > 10) {
-                            $title = mb_substr($post->post_title, 0, 10);
+                        <?php if (mb_strlen($post->post_title) > 29) {
+                            $title = mb_substr($post->post_title, 0, 29);
                             echo $title . '...';
                         } else {
                             echo $post->post_title;
