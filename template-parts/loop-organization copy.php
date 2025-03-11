e430515263add0d7df237f38d0bd6f1b4437217d
04limited-Sazabys <kontaku3315@gmail.com>
2025-03-11 14:00:01 +0900

    主催団体カード型修正

--- template-parts/loop-organization copy.php
+++ template-parts/loop-organization copy.php
@@ -1,53 +1,51 @@
-<div class="card_wrap">
-    <a href="<?php the_permalink(); ?>">
-        <div class="orgcard_shape">
-            <div class="card_inner">
-                <div>
-                    <?php if (has_post_thumbnail()) : ?>
-                        <!-- アイキャッチ画像があった場合は、表示 -->
-                        <?php the_post_thumbnail('medium'); ?>
-                    <?php else : ?>
-                        <!-- アイキャッチ画像が設定していない場合は、noimage.pngを表示 -->
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage.png" alt="">
-                    <?php endif; ?>
-                </div>
+<a class="orgcard_wrap" href="<?php the_permalink(); ?>">
+    <div class="orgcard_inner">
+        <div>
+            <div>
+                <?php if (has_post_thumbnail()) : ?>
+                    <!-- アイキャッチ画像があった場合は、表示 -->
+                    <?php the_post_thumbnail('medium'); ?>
+                <?php else : ?>
+                    <!-- アイキャッチ画像が設定していない場合は、noimage.pngを表示 -->
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/noimage.png" alt="">
+                <?php endif; ?>
+            </div>
 
-                <div class="card_text">
-                    <h3><?php the_title(); ?></h3>
-                    <div class="orgcard_line">
-                    </div>
-                    <div>
-                        <span class="card_subtitle">所在地</span>
-                        <div>
-                            <span>
-                                <?php if (mb_strlen(get_field('address')) > 20) {
-                                    $address = mb_substr(get_field('address'), 0, 20);
-                                    echo $address . '…';
-                                } else {
-                                    echo get_field('address');
-                                } ?>
-                            </span>
-                        </div>
-                    </div>
-                    <span class="card_subtitle">電話番号</span>
+            <div class="orgcard_text">
+                <h3><?php the_title(); ?></h3>
+                <div class="orgcard_line">
+                </div>
+                <div>
+                    <span class="card_subtitle">所在地</span>
                     <div>
                         <span>
-                            <?php the_field('tel'); ?>
+                            <?php if (mb_strlen(get_field('address')) > 20) {
+                                $address = mb_substr(get_field('address'), 0, 20);
+                                echo $address . '…';
+                            } else {
+                                echo get_field('address');
+                            } ?>
                         </span>
                     </div>
-                    <?php if (get_field('hour')): ?>
-                        <div>
-                            <span class="card_subtitle">営業時間</span>
-                            <div>
-                                <span>
-                                    <?php the_field('hour') ?>
-                                </span>
-
-                            </div>
-                        </div>
-                    <?php endif; ?>
                 </div>
+                <span class="orgcard_subtitle">電話番号</span>
+                <div>
+                    <span>
+                        <?php the_field('tel'); ?>
+                    </span>
+                </div>
+                <?php if (get_field('hour')): ?>
+                    <div>
+                        <span class="orgcard_subtitle">営業時間</span>
+                        <div>
+                            <span>
+                                <?php the_field('hour') ?>
+                            </span>
+
+                        </div>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
-    </a>
-</div>
+    </div>
+</a>
