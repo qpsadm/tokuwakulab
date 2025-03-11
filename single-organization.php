ed96b7477218f92f5dec59d2e439bd9d85381e2a
04limited-Sazabys <kontaku3315@gmail.com>
2025-03-11 19:10:29 +0900

    主催団体詳細ページ完成？

--- single-organization.php
+++ single-organization.php
@@ -2,7 +2,7 @@
 
 <main class="pc_space">
     <!-- ページタイトル -->
-    <section class="page_top">
+    <section class="orgpage_top">
         <h2 class="page_title">主催団体詳細</h2>
     </section>
     <!-- パンくずリスト -->
@@ -10,147 +10,154 @@
 
     <div class="inner">
         <?php if (have_posts()): ?>
-        <?php while (have_posts()) : the_post(); ?>
-        <section class="org_top">
-            <h2 class="org_name"><?php the_title(); ?></h2>
-            <!-- キービジュアル -->
-            <div class="top_kv_wrap">
-                <ul class="slider_kv">
-                    <li><?php
+            <?php while (have_posts()) : the_post(); ?>
+                <section class="org_top">
+                    <h2 class="org_name"><?php the_title(); ?></h2>
+                    <!-- キービジュアル -->
+                    <div class="top_kv_wrap">
+                        <ul class="slider_kv">
+                            <li><?php
                                 $pic = get_field('image01');
                                 $pic_url = $pic['url'];
                                 ?>
-                        <img src="<?php echo $pic_url; ?>" alt="キービジュアル">
-                    </li>
-                    <?php if (get_field('image02')): ?>
-                    <li><?php
+                                <img src="<?php echo $pic_url; ?>" alt="キービジュアル">
+                            </li>
+                            <?php if (get_field('image02')): ?>
+                                <li><?php
                                     $pic = get_field('image02');
                                     $pic_url = $pic['url'];
                                     ?>
-                        <img src="<?php echo $pic_url; ?>" alt="キービジュアル">
-                    </li>
-                    <?php endif; ?>
-                    <?php if (get_field('image03')): ?>
-                    <li><?php
+                                    <img src="<?php echo $pic_url; ?>" alt="キービジュアル">
+                                </li>
+                            <?php endif; ?>
+                            <?php if (get_field('image03')): ?>
+                                <li><?php
                                     $pic = get_field('image03');
                                     $pic_url = $pic['url'];
                                     ?>
-                        <img src="<?php echo $pic_url; ?>" alt="キービジュアル">
-                    </li>
-                    <?php endif; ?>
-                    <?php if (get_field('image04')): ?>
-                    <li>
-                        <?php
+                                    <img src="<?php echo $pic_url; ?>" alt="キービジュアル">
+                                </li>
+                            <?php endif; ?>
+                            <?php if (get_field('image04')): ?>
+                                <li>
+                                    <?php
                                     $pic = get_field('image04');
                                     $pic_url = $pic['url'];
                                     ?>
-                        <img src="<?php echo $pic_url; ?>" alt="キービジュアル">
-                    </li><?php endif; ?>
-                </ul>
-            </div>
+                                    <img src="<?php echo $pic_url; ?>" alt="キービジュアル">
+                                </li><?php endif; ?>
+                        </ul>
+                    </div>
 
-            <!-- 団体紹介文 -->
-            <div class="org_description">
-                <p><?php the_field('info'); ?></p>
-            </div>
-        </section>
+                    <!-- SNSのアイコン -->
+                    <div class="org_snsicons">
 
-        <div class="section_line"></div>
-        <!-- 主催団体の基本情報 -->
-        <section>
-            <h3 class="sub_title"><?php the_title(); ?>の基本情報</h3>
-            <table class="org_table">
-                <tr>
-                    <th>団体名</th>
-                    <td><?php the_title(); ?></td>
-                </tr>
+                        <!-- facebookのアイコン -->
+                        <?php if (get_field('facebook')): ?>
+                            <a target="_blank" href="<?php the_field('facebook') ?>">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/facebook_icon.svg" alt="facebookのアイコン">
+                            </a>
+                        <?php endif; ?>
 
-                <tr>
-                    <th>代表者名</th>
-                    <td><?php the_field('name'); ?></td>
-                </tr>
+                        <!-- instagramのアイコン -->
+                        <?php if (get_field('instagram')): ?>
+                            <a target="_blank" href="<?php the_field('instagram') ?>">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/instagram_icon.svg" alt="instagramのアイコン">
+                            </a>
+                        <?php endif; ?>
 
-                <tr>
-                    <th>郵便番号</th>
-                    <td><?php the_field('postcode'); ?></td>
-                </tr>
-                <tr>
-                    <th>所在地</th>
-                    <td><?php the_field('address'); ?></td>
-                </tr>
-                <tr>
-                    <th>電話番号</th>
-                    <td><?php the_field('tel'); ?></td>
-                </tr>
-                <tr>
-                    <th>営業時間</th>
-                    <td><?php the_field('hour'); ?></td>
-                </tr>
-                <tr>
-                    <th>FAX</th>
-                    <td><?php the_field('fax'); ?></td>
-                </tr>
-                <tr>
-                    <th>メールアドレス</th>
-                    <td><?php the_field('email'); ?></td>
-                </tr>
-                <tr>
-                    <th>URL</th>
-                    <td><a target="_brank" href="<?php the_field('url'); ?>"><?php the_field('url'); ?></a></td>
-                </tr>
+                        <!-- Xのアイコン -->
+                        <?php if (get_field('x')): ?>
+                            <a target="_blank" href="<?php the_field('x') ?>">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/x_icon.svg" alt="Xのアイコン">
+                            </a>
+                        <?php endif; ?>
 
-                <!-- SNSは入力がない場合は表示しない -->
-                <!-- インスタ -->
-                <?php if (get_field('instagram')): ?>
-                <tr>
-                    <th>Instagram</th>
-                    <td><a target="_brank" href="<?php the_field('instagram'); ?>"><?php the_field('instagram'); ?></a></td>
-                </tr>
-                <?php endif; ?>
-                <!-- X -->
-                <?php if (get_field('x')): ?>
-                <tr>
-                    <th>X</th>
-                    <td><a target="_brank" href="<?php the_field('x'); ?>"><?php the_field('x'); ?></a></td>
-                </tr>
-                <?php endif; ?>
-                <!-- facebook -->
-                <?php if (get_field('facebook')): ?>
-                <tr>
-                    <th>facebook</th>
-                    <td><a target="_brank" href="<?php the_field('facebook'); ?>"><?php the_field('facebook'); ?></a></td>
-                </tr>
-                <?php endif; ?>
+                        <!-- tiktokのアイコン -->
+                        <?php if (get_field('tiktok')): ?>
+                            <a target="_blank" href="<?php the_field('tiktok') ?>">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/tiktok_icon.svg" alt="Xのアイコン">
+                            </a>
+                        <?php endif; ?>
 
-                <!-- Line -->
-                <?php if (get_field('line')): ?>
-                <tr>
-                    <th>LINE</th>
-                    <td><a target="_brank" href="<?php the_field('line'); ?>"><?php the_field('line'); ?></a></td>
-                </tr>
-                <?php endif; ?>
+                        <!-- youtubeのアイコン -->
+                        <?php if (get_field('youtube')): ?>
+                            <a target="_blank" href="<?php the_field('youtube') ?>">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/youtube_icon.svg" alt="youtubeのアイコン">
+                            </a>
+                        <?php endif; ?>
 
-                <!-- Youtube -->
-                <?php if (get_field('youtube')): ?>
-                <tr>
-                    <th>Youtube</th>
-                    <td><a target="_brank" href="<?php the_field('youtube'); ?>"><?php the_field('youtube'); ?></td>
-                </tr>
-                <?php endif; ?>
-                <tr>
-                    <th>備考</th>
-                    <td><?php the_field('remarks'); ?></td>
-                </tr>
+                        <!-- LINEのアイコン -->
+                        <?php if (get_field('line')): ?>
+                            <a target="_blank" href="<?php the_field('line') ?>">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/line_icon.svg" alt="LINEのアイコン">
+                            </a>
+                        <?php endif; ?>
 
-            </table>
-        </section>
-        <?php endwhile; ?>
+                    </div>
+
+
+                    <!-- 団体紹介文 -->
+                    <div class="org_description">
+                        <p><?php the_field('info'); ?></p>
+                    </div>
+                </section>
+
+                <div class="org_section"></div>
+                <!-- 主催団体の基本情報 -->
+                <section>
+                    <h3 class="sub_title"><?php the_title(); ?>の基本情報</h3>
+                    <table class="org_table">
+                        <tr>
+                            <th>団体名</th>
+                            <td><?php the_title(); ?></td>
+                        </tr>
+
+                        <tr>
+                            <th>代表者名</th>
+                            <td><?php the_field('name'); ?></td>
+                        </tr>
+
+                        <tr>
+                            <th>郵便番号</th>
+                            <td><?php the_field('postcode'); ?></td>
+                        </tr>
+                        <tr>
+                            <th>所在地</th>
+                            <td><?php the_field('address'); ?></td>
+                        </tr>
+                        <tr>
+                            <th>電話番号</th>
+                            <td><?php the_field('tel'); ?></td>
+                        </tr>
+                        <tr>
+                            <th>営業時間</th>
+                            <td><?php the_field('hour'); ?></td>
+                        </tr>
+                        <tr>
+                            <th>FAX</th>
+                            <td><?php the_field('fax'); ?></td>
+                        </tr>
+                        <tr>
+                            <th>メールアドレス</th>
+                            <td><?php the_field('email'); ?></td>
+                        </tr>
+                        <tr>
+                            <th>URL</th>
+                            <td><a target="_brank" href="<?php the_field('url'); ?>"><?php the_field('url'); ?></a></td>
+                        </tr>
+                        <tr>
+                            <th>備考</th>
+                            <td><?php the_field('remarks'); ?></td>
+                        </tr>
+
+                    </table>
+                </section>
+            <?php endwhile; ?>
         <?php endif; ?>
 
-        <div class="section_line"></div>
-
         <!-- マップ -->
-        <section>
+        <section class="org_section">
             <h3 class="sub_title">所在地マップ</h3>
             <div class="map">
                 <?php echo get_field('map'); ?>
@@ -159,7 +166,7 @@
         <div class="section_line"></div>
 
         <!-- 開催予定イベント３件表示 -->
-        <section>
+        <section class="org_section">
             <h3 class="sub_title">開催予定のイベント</h3>
 
             <?php
@@ -206,15 +213,15 @@
             <ul class="top_event_list">
                 <!-- イベントループの開始 -->
                 <?php if ($the_query->have_posts()) { ?>
-                <?php while ($the_query->have_posts()) { ?>
-                <?php $the_query->the_post(); ?>
+                    <?php while ($the_query->have_posts()) { ?>
+                        <?php $the_query->the_post(); ?>
 
-                <li>
-                    <!-- テンプレートパーツloop-event.phpを読み込む -->
-                    <?php get_template_part('template-parts/loop', 'event') ?>
-                </li>
+                        <li>
+                            <!-- テンプレートパーツloop-event.phpを読み込む -->
+                            <?php get_template_part('template-parts/loop', 'event') ?>
+                        </li>
 
-                <?php } ?>
+                    <?php } ?>
                 <?php } else { // 関連イベントがない場合のメッセージ
                     echo '<p>開催予定のイベントはありません。</p><br>';
                 }
@@ -225,10 +232,9 @@
             </div>
         </section>
 
-        <div class="section_line"></div>
 
         <!-- 過去開催イベントのタイトル表示 -->
-        <section class="column_content_wrap">
+        <section class="column_content_wrap org_section">
             <h3 class="sub_title">過去開催イベント</h3>
             <div class="column_recomend_list">
                 <?php
@@ -265,10 +271,10 @@
                 if ($related_post->have_posts()) {
                     while ($related_post->have_posts()) {
                         $related_post->the_post(); ?>
-                <span><?php the_field("date_start"); ?></span>
-                <a href="<?php the_permalink(); ?>">
-                    <span><?php the_title(); ?></span>
-                </a>
+                        <span><?php the_field("date_start"); ?></span>
+                        <a href="<?php the_permalink(); ?>">
+                            <span><?php the_title(); ?></span>
+                        </a>
                 <?php }
                 } else {
                     // 関連投稿がない場合のメッセージ
@@ -280,10 +286,9 @@
                 ?>
             </div>
         </section>
-        <div class="section_line"></div>
 
         <!-- 関連コラム記事３件タイトルのみ表示 -->
-        <section class="column_content_wrap">
+        <section class="column_content_wrap org_section">
 
             <h3 class="sub_title">関連コラム記事</h3>
             <div class="column_recomend_list">
@@ -296,14 +301,14 @@
                 ];
                 $the_query = new WP_Query($args);
                 if ($the_query->have_posts()): ?>
-                <?php while ($the_query->have_posts()): ?>
-                <?php $the_query->the_post(); ?>
-                <span><time datetime="<?php the_time('Y/m/d'); ?>"><?php the_time('Y/m/d/(l)'); ?></time></span>
-                <a href="<?php the_permalink(); ?>">
-                    <span><?php the_title(); ?></span></a>
+                    <?php while ($the_query->have_posts()): ?>
+                        <?php $the_query->the_post(); ?>
+                        <span><time datetime="<?php the_time('Y/m/d'); ?>"><?php the_time('Y/m/d/(l)'); ?></time></span>
+                        <a href="<?php the_permalink(); ?>">
+                            <span><?php the_title(); ?></span></a>
 
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
                 <?php endif; ?>
             </div>
 
@@ -312,9 +317,6 @@
         <div class="btnwrap_wrap">
             <a href="<?php echo home_url("/column/"); ?>" class="btn_wrap">コラム一覧へ</a>
         </div>
-
-        <div class="section_line"></div>
-
     </div>
 </main>
 
